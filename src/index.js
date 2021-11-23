79d61176f1a9e54ee6fe541b1d60f6406df7797e
Vincent Dhennin <vincent.dhennin7@gmail.com>
2021-11-23 15:22:48 +0100

    kickstart structure

--- src/index.js
+++ src/index.js
@@ -6,6 +6,43 @@
 
 // See: scanner/types/scanner.d.ts -> Dependency.metadata
 export async function extractAndOptimizeUsers(dependencyMetadata) {
-  console.log(dependencyMetadata);
-  // DO WORK HERE!
+  if (!'author', 'maintainers', 'publishers' in dependencyMetadata) {
+    throw new TypeError(
+      "There is a problem with your object parameters, please check if you have 'author', 'maintainers','publishers'"
+    );
+  }
+  const { author, maintainers, publishers } = dependencyMetadata;
+  const formattedResponse = formatResponse(author, maintainers, publishers);
+}
+
+function splitAuthorNameEmail(author) {
+  const indexStartEmail = author.search(/[<]/g);
+  const indexEndEmail = author.search(/[>]/g)
+  if (!indexStartEmail && !indexEndEmail) {
+    return author
+  }
+  return {
+    name: author.substring(0, indexStartEmail).trim(),
+    email: author.substring(indexStartEmail, indexEndEmail).trim()
+  }
+}
+
+// Format response
+function formatResponse(author, maintainers, publishers) {
+  const authors = [];
+
+  const objectAuthor = (name) => {
+    return {
+      name,
+      email: '',
+    };
+  }
+
+  for (const item of author) {
+    if ('email' in item) {
+      authors.push(splitAuthorNameEmail(item))
+    } else {
+      authors.push(objectAuthor(item))
+    }
+  }
 }
