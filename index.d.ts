67f1d32e815d7c713e62ac27edab3b6d827bd498
Vincent Dhennin <vincent.dhennin7@gmail.com>
2022-02-06 20:44:12 +0100

    chore: types

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,15 @@
 import { Maintainer } from "@npm/types";
 import { Scanner } from "@nodesecure/scanner";
 
-export function extractAndOptimizeUsers(maintainers: Maintainer): Maintainer[] | [];
-export function extractAllAuthorsFromLibrary(library: Scanner.Payload): Maintainer[] | [];
+export function extractAndOptimizeUsers(maintainers: Maintainer): authorsResponse
+export function extractAllAuthorsFromLibrary(library: Scanner.Payload): authorsResponse
+
+export interface authorsResponse {
+  authors: Maintainer[] | [],
+  flags: flagAuthor[] | []
+}
+export interface flagAuthor {
+  name: string,
+  email: string,
+  foundIn: string[] | null
+}
