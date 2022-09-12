b0778e7fbdd99769050fb1915244c3fe9eb530e5
rossb0b <nicolas.hallaert.pro@gmail.com>
2022-09-12 19:07:08 +0200

    fix(index.d.ts): updated type related to activity data & cleaned names

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,32 @@
 // Import Third-party Dependencies
 import { Scanner } from "@nodesecure/scanner";
 
-export function extractAllAuthorsFromLibrary(library: Scanner.Payload, opts: options): Promise<authorResponse[]>
+export function extractAllAuthorsFromLibrary(library: Scanner.Payload, opts: ExtractAllAuthorsFromLibraryOptions): Promise<AuthorResponse[]>
 
-export interface options {
-  flags: flagAuthor[],
+export interface ExtractAllAuthorsFromLibraryOptions {
+  flags: FlagAuthor[],
   domainInformations: boolean,
 }
 
-export interface authorResponse {
+export type Package = Record<string, any> & {
+  havePublishRecently: boolean;
+  hasBeenActiveOnGithubRepo: boolean | null;
+}
+
+export interface AuthorResponse {
   name?: string;
   email?: string;
   url?: string;
-  flagged: boolean,
-  packages: unknown[],
+  flagged: boolean;
+  packages: Package[];
+  hasBeenActiveOnGithub: boolean | null;
   domain?: {
-    expirationDate?: string,
-    mxRecords?: unknown[],
+    expirationDate?: string;
+    mxRecords?: string[];
   }
 }
-export interface flagAuthor {
-  name: string,
-  email: string,
+
+export interface FlagAuthor {
+  name: string;
+  email: string;
 }
